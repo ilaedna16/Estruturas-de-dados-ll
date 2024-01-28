918152838ed9f70d5f01ffd1c2a68c1020f1b36a
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-01-28 08:53:08 -0300

    Update problema 9. Escreva um programa para converter e imprimir os caracteres para os valores ASCII de 0 a 127. O programa deve imprimir 10 caracteres por linha.c

--- problema 9. Escreva um programa para converter e imprimir os caracteres para os valores ASCII de 0 a 127. O programa deve imprimir 10 caracteres por linha.c
+++ problema 9. Escreva um programa para converter e imprimir os caracteres para os valores ASCII de 0 a 127. O programa deve imprimir 10 caracteres por linha.c
@@ -1,19 +1,21 @@
 #include<stdio.h> 
 #include<stdlib.h> 
-#include<locale.h>
+#include<locale.h> 
 //Escreva um programa para converter e imprimir os caracteres
 //para os valores ASCII de 0 a 127. O programa deve imprimir 10 caracteres
 //por linha.
 
 int main() {
+    setlocale (LC_ALL, "portuguese"); 
     int i;
+    printf("-------------------------------");
+    printf("Caracteres ASCII de 0 a 127:\n ");
+   
 
-    for (i = 0; i <= 127; i++) {
-        
-        printf("%c:%d\t", i, i);
-    
-        if ((i + 1) % 10 == 0)
-            printf("\n");
+    for (i=0; i<=128; i++) { 
+        printf("%3d: %c",i); //quebra de linha 
+        if ((i+1) % 10==0) 
+        printf ("\n"); 
     }
     return 0;
 }
