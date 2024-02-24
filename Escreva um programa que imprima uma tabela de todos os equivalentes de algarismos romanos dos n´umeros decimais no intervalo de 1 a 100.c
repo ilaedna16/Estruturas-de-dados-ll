1728fa3ffdaf795588d2758d5079ac8a04a25028
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-02-24 13:19:04 -0300

    Create Escreva um programa que imprima uma tabela de todos os equivalentes de algarismos romanos dos n´umeros decimais no intervalo de 1 a 100.c

---
+++ Escreva um programa que imprima uma tabela de todos os equivalentes de algarismos romanos dos n´umeros decimais no intervalo de 1 a 100.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Escreva um programa que imprima uma tabela de todos os equivalentes de algarismos romanos
+//dos n´umeros decimais no intervalo de 1 a 100.
+
+
+
+    void converteParaRomano (int numero){ 
+        if (numero <1 || numero> 100){ 
+            printf("Numero fora do intervalo(1 a 100)\n"); 
+            return; 
+
+        }
+        printf("%d3d",numero);  
+
+        while (numero>=100){ 
+            printf("C"); 
+              numero-=100; 
+        }
+
+                    if (numero>=90){ 
+                    printf("XC");
+                        numero-=90; 
+                    }  
+
+                        while (numero>=50){ 
+                        printf("L"); 
+                        numero-=50; 
+                        } 
+
+                            if (numero>=40){ 
+                            printf("XL");
+                            numero-=40;
+                        } 
+
+            while (numero>=10){ 
+            printf("X"); 
+                    numero-=10; 
+            }
+            if (numero>=9){ 
+                 printf("IX");
+                    numero-=9;
+            }
+                    while (numero>=5){ 
+                      printf("V"); 
+                             numero-=5;
+                    }
+                                if (numero>=4){ 
+                                     printf("IV");
+                                          numero-=4;
+
+                                           }
+                                    while (numero>=1){ 
+                                        printf("I"); 
+                                            numero-=1; 
+                                    } 
+
+ int main() { 
+
+    printf("Tabela de equivalentes de algarismos romanos para numeros decimais(1 a 100):\n"); 
+         for(int i=1; i<=100; i++) { 
+             converteParaRomano(i); 
+         } 
+     
+return 0; 
+} 
